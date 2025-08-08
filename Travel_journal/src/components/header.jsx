e8cd7e552a75af0c1243c3145b5a5aefa28f2442
EmilijaKuzmanovicc <emilija.kuzmanovic@shindiristudio.com>
2025-08-08 11:15:59 +0200

    Created navbar file.

---
+++ Travel_journal/src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav className="navbar">
+      <img src="/src/assets/globe.png" className="globeImg" />
+      <div className="globeText">my travel journal.</div>
+    </nav>
+  );
+}
--- Travel_journal/src/main.jsx
+++ Travel_journal/src/main.jsx
@@ -1,3 +1,7 @@
 import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")).render();
+import Header from "./components/header";
+createRoot(document.getElementById("root")).render(
+  <>
+    <Header />
+  </>
+);
